27a19538023d758c365dc7732bcdf1e9f0c57b1a
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-02-16 13:42:15 +0100

    Init project and install depenencies

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import { Canvas } from "@react-three/fiber";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Canvas>
+    <Scene />
+  </Canvas>
+);
